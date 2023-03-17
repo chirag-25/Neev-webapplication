1bbc934544cb125f2fb419979309954e22018d2a
RahulRai02 <rahulr0211@gmail.com>
2023-03-17 11:31:01 +0530

    Rendered table on page

--- app/app.py
+++ app/app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+import yaml
 
 app=Flask(__name__)
 
+# Configure the database
+db = yaml.load(open('db.yaml'), Loader=yaml.Loader)
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+print("This is my db", mysql)
+
 @app.route("/")
-def home():
+def home():    
     return render_template("home.html")
 
 @app.route("/login",methods=['GET','POST'])
@@ -19,16 +31,59 @@ def admin():
 
 @app.route("/admin/user",methods=['POST','GET'])
 def user():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM Beneficiary")
+    if (resultValue > 0):
+        userDetails = cur.fetchall()
+    # print("this is result value", resultValue)
     if(request.method=='POST'):
-        print(request.json)
+
+        # print("userdetails", userDetails)
+            
+        print("REQUEST", request.json)
         form_data=request.json
+        print("form data is", form_data)
+        
         if(form_data['signal']=='search'):
             print("this is search query")
         elif(form_data['signal']=='editUser'):
             print("this is edit query")  
         elif(form_data['signal']=='addUser'):
+            print("ADD DATA")
+            # return redirect(url_for('admin/user.html'))
             print("add new data")     
-    return render_template('admin/user.html')
+    return render_template('admin/user.html', userDetails=userDetails)
+    # return render_template('admin/user.html')
 
+### Add new user
+@app.route("/",methods=['POST'])
+def insert():
+    if (request.method == 'POST'):
+        aadhar = request.form['aadhar']
+        
+        name = request.form['name']
+        print(name)
+        dob = request.form['dob']
+        education = request.form['education']
+        martial = request.form['martial']
+        gender = request.form['gender']
+        employed = request.form['employed']
+        
+        print(dob)
+        print(education)
+        print(martial)
+        print(gender)
+        
+        print(employed)
+        # print("here", aadhar)
+        cur = mysql.connection.cursor()
+        
+        query = "INSERT INTO Beneficiary (aadhar_id, name, date_of_birth, gender, marital_status, education, employed) VALUES (%s %s %s %s %s %s %s)"
+        # cur.execute(, (aadhar, name, dob, education, maritalstatus,  gender, employment_status) )  
+        cur.execute(query, (aadhar, name, dob, gender, martial, education, employed))               
+        mysql.connection.commit()
+        cur.close()
+        # return redirect('/admin/user.html')
+    return render_template('admin/user')
 if __name__=='__main__':
     app.run(debug=True)
